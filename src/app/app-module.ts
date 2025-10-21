932ad07e6575d2f1e47c45293a7a6ba3b5afa534
labohkip81 <labankiplagat81@gmail.com>
2025-10-21 07:00:33 +0300

    updates.

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { MaterialModule } from './modules/material/material-module';
+import { Home } from './pages/home/home';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Home
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners()
